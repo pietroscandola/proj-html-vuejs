f30df6dfd42138f8b2604593ca4dcc4444e93dff
pietroscandola <pietro.scandolavr@gmail.com>
2022-02-21 00:41:17 +0100

    add Footer

--- src/components/footer/BottomFooter.vue
+++ src/components/footer/BottomFooter.vue
@@ -1,11 +1,58 @@
 <template>
   <div class="bg-image">
-    <div class="container"></div>
+    <div class="container h-100">
+      <div class="row h-100 align-items-center">
+        <div class="col-6">
+          <p>
+            &copy; Copyright 2012 - 2020| Avada Theme by
+            <span>ThemeFusion</span> | All Rights Reserved | Powered by
+            <span>Wordpress</span>
+          </p>
+        </div>
+        <div class="col-6 d-flex justify-content-end">
+          <ul class="d-flex">
+            <li><i class="fa-brands fa-facebook-f mx-2"></i></li>
+            <li><i class="fa-brands fa-twitter mx-2"></i></li>
+            <li><i class="fa-brands fa-youtube mx-2"></i></li>
+            <li><i class="fa-brands fa-instagram mx-2"></i></li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "BottomFooter",
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import "../../assets/scss/style.scss";
+.bg-image {
+  min-width: 100%;
+  height: 100px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  background-color: black;
+}
+p {
+  font-size: 13px;
+  color: $color_gray;
+}
+
+span {
+  color: $color_green;
+}
+
+li {
+  list-style-type: none;
+}
+
+.fa-brands {
+  font-size: 1.3rem;
+  color: white;
+}
+</style>
--- src/components/footer/MainFooter.vue
+++ src/components/footer/MainFooter.vue
@@ -1,11 +1,12 @@
 <template>
   <section class="bg-image">
     <div class="container h-100">
-      <div class="row align-items-center h-100">
+      <div class="row h-100">
         <div class="col-3">
           <img
             src="../../assets/img/avada-drivers-logo-1x.png"
             alt="Logo Avada"
+            class="py-3"
           />
           <p>
             Avada Driving School is the right choice for experienced drivers
@@ -14,32 +15,56 @@
           </p>
         </div>
         <div class="col-3">
-          <h4>CONTACT DETAILS</h4>
-          <ul>
-            <li><i class="fa-solid fa-house-chimney"></i></li>
-            <li><i class="fa-solid fa-phone-flip"></i></li>
-            <li><i class="fa-solid fa-envelope"></i></li>
-            <li></li>
+          <h4 class="py-3">CONTACT DETAILS</h4>
+          <ul class="contacts">
+            <li class="py-2">
+              <i class="fa-solid fa-house-chimney pe-3"></i>
+              <span>12345 North Main Street, New York, NY 555555</span>
+            </li>
+            <li class="py-2">
+              <i class="fa-solid fa-phone-flip pe-3"></i>
+              <span>1.800.555.6789</span>
+            </li>
+            <li class="py-2">
+              <i class="fa-solid fa-envelope pe-3"></i>
+              <span>info@your-domain.com</span>
+            </li>
+            <li class="py-2">
+              <i class="fa-solid fa-link pe-3"></i>
+              <span>Theme-Fusion</span>
+            </li>
           </ul>
         </div>
         <div class="col-3">
-          <h4>CONTACT DETAILS</h4>
+          <h4 class="py-3">CONTACT DETAILS</h4>
           <ul>
-            <li><i class="fa-solid fa-circle-arrow-right"></i>Pass Plus</li>
-            <li>
-              <i class="fa-solid fa-circle-arrow-right"></i>Intensive Course
+            <li class="py-2">
+              <i class="fa-solid fa-circle-arrow-right pe-3"></i
+              ><span class="contact-details">Pass Plus</span>
             </li>
-            <li><i class="fa-solid fa-circle-arrow-right"></i>Automatic</li>
-            <li>
-              <i class="fa-solid fa-circle-arrow-right"></i>Instructor Training
+            <li class="py-2">
+              <i class="fa-solid fa-circle-arrow-right pe-3"></i
+              ><span class="contact-details">Intensive Course</span>
+            </li>
+            <li class="py-2">
+              <i class="fa-solid fa-circle-arrow-right pe-3"></i
+              ><span class="contact-details">Automatic</span>
+            </li>
+            <li class="py-2">
+              <i class="fa-solid fa-circle-arrow-right pe-3"></i
+              ><span class="contact-details">Instructor Training</span>
             </li>
           </ul>
         </div>
         <div class="col-3">
-          <figure>
-            <img src="../../assets/img/footer-ad-grid.png" alt="Advertising" />
+          <figure class="d-flex justify-content-end">
+            <img
+              src="../../assets/img/footer-ad-grid.png"
+              class="img-fluid"
+              alt="img_footer"
+            />
           </figure>
-          <div>
+          <div class="d-flex justify-content-center">
             <button class="button text-center">BOOK NOW</button>
           </div>
         </div>
@@ -55,12 +80,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../../assets/scss/style.scss";
 .bg-image {
   background-image: url("../../assets/img/footer-background-color.jpg");
   min-width: 100%;
-  height: 400px;
+  height: 450px;
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
+}
+.row {
+  padding: 70px;
+}
+
+li {
+  list-style-type: none;
+}
+h4 {
+  color: white;
+}
+.fa-circle-arrow-right,
+.contact-details {
+  color: $color_green;
+}
+
+.contacts,
+p {
+  color: $color_gray;
 }
 </style>
